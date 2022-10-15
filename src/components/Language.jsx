b9e450853300a5145a3dc24c3da576871668837c
eyupGuc <meydanguresi@gmail.com>
2022-10-15 13:22:45 +0300

    v

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
 const Language = ({ name, options, img }) => {
-//   console.log(Lang);
+  const [showDesc, setShowDesc] = useState(false);
+  console.log(showDesc)
+  //   console.log(Lang);
   return (
-    <Container style={{background:"peachpuff"}} className="p-4 rounded-2">
+    <Container
+      style={{ background: "peachpuff" }}
+      className="p-4 rounded-2"
+      onClick={() => setShowDesc(!showDesc)}
+    >
       <Container>
         <Image src={img} width="70%"></Image>
         <h3 className="display-6">{name}</h3>
